5ef098b9e3830f170d301f17de67a7d2dcd9d692
mayyaannkk <mayankpant.dev@gmail.com>
2025-08-20 17:18:45 +0530

    refactor: better way for adding UserDetailsService bean

--- src/main/java/com/mayank/spring_security/config/WebSecurityConfig.java
+++ src/main/java/com/mayank/spring_security/config/WebSecurityConfig.java
@@ -1,27 +1,12 @@
 package com.mayank.spring_security.config;
 
-import com.mayank.spring_security.repositories.UserRepository;
-import com.mayank.spring_security.services.JpaUserDetailsService;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@AllArgsConstructor
 public class WebSecurityConfig {
-
-    private final UserRepository userRepository;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var uds = new JpaUserDetailsService(userRepository);
-        return uds;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
