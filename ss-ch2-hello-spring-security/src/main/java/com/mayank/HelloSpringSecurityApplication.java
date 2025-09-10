068f0c9ce4d2676395acfb1d912eab402a32a155
mayyaannkk <mayankpant.dev@gmail.com>
2025-09-10 17:37:27 +0530

    chore: split into multi-module project

---
+++ ss-ch2-hello-spring-security/src/main/java/com/mayank/HelloSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.mayank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringSecurityApplication.class, args);
+    }
+}
+
