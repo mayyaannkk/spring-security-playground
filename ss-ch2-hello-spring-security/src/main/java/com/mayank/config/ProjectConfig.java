db0703d77f2aee0f81f49ee27c78cd37810c45f6
mayyaannkk <mayankpant.dev@gmail.com>
2025-09-12 12:28:28 +0530

    feat: added security filter chain config

--- ss-ch2-hello-spring-security/src/main/java/com/mayank/config/ProjectConfig.java
+++ ss-ch2-hello-spring-security/src/main/java/com/mayank/config/ProjectConfig.java
@@ -2,13 +2,18 @@ package com.mayank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService() {
@@ -27,4 +32,16 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+//                c -> c.anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+
 }
